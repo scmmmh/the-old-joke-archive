14553f21bf4e280f38a32a593c7686a7943023b8
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-02-25 16:00:16 +0000

    Added pre/post scripts

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ -f 'pre-build.sh' ]; then
+   ./pre-build.sh
+fi
+
 git pull
-export PIPENV_VENV_IN_PROJECT=True
+#export PIPENV_VENV_IN_PROJECT=True
 pipenv install
 pipenv run pip install -e .
-yarn install
-node_modules/.bin/gulp
 
 pipenv run toja init-db
+
+if [ -f 'post-build.sh' ]; then
+   ./post-build.sh
+fi
