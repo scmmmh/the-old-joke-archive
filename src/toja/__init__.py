de3d2ea40a9c653a6c0290e0b15d856f469b23b2
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-07-04 20:00:06 +0200

    Code refactoring

--- src/toja/__init__.py
+++ src/toja/__init__.py
@@ -6,6 +6,7 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
+    config.commit()
     config.include('.models')
     config.include('.routes')
     config.scan()
--- src/toja/routes.py
+++ src/toja/routes.py
@@ -1,3 +1,10 @@
+from pyramid_jinja2.filters import route_url_filter, static_url_filter
+
+
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('root', '/')
+
+    # Jinja2 configuration
+    config.get_jinja2_environment().filters['static_url'] = static_url_filter
+    config.get_jinja2_environment().filters['route_url'] = route_url_filter
