25b7db42e8a2a978d81f73798feaeca7b495f5cb
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-07-16 17:31:01 +0100

    Added edit links to the user profile

--- src/toja/permissions.py
+++ src/toja/permissions.py
@@ -13,11 +13,13 @@ PERMISSIONS = OrderedDict((('admin.view', 'Access the Admin Interface'),
                            ('users.edit_permissions', 'Edit all User\'s Permissions'),
                            ('users.delete', 'Delete any Users'),
                            ('sources.new', 'Add new Sources'),
+                           ('sources.view', 'View all Sources'),
                            ('sources.edit', 'Edit all Sources'),
                            ('sources.delete', 'Delete any Source')))
 GROUPS = OrderedDict((('admin', ('admin.view',
                                  'users.list', 'users.edit', 'users.edit_permissions', 'users.delete')),
-                      ('data provider', ('sources.new', ))))
+                      ('data provider', ('sources.new', )),
+                      ('data admin', ('sources.new', 'sources.view', 'sources.edit', 'sources.delete'))))
 PERMISSIONS_GROUPS = dict([permission, group] for group, permissions in GROUPS.items() for permission in permissions)
 
 OR = 1
