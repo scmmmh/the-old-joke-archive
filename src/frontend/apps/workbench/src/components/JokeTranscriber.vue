ffc10e8a6dffefc85c538f7a3279ccc2c4bddc22
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-07-26 13:00:47 +0100

    Basic integration of tiptap editor

--- src/frontend/apps/workbench/src/components/JokeTranscriber.vue
+++ src/frontend/apps/workbench/src/components/JokeTranscriber.vue
@@ -1,16 +1,42 @@
 <template>
-    <div v-if="selectedJoke">
-    </div>
-    <div v-else>
-        <p>Please select a joke from the list to transcribe and annotate it.</p>
+    <div class="joke-transcriber">
+        <nav>
+            <ul role="menu" class="menu">
+                <li role="presentation">
+                    <a role="menuitem">Title</a>
+                </li>
+            </ul>
+        </nav>
+        <editor-content :editor="editor"></editor-content>
+        <div class="overlay" v-if="!selectedJoke">
+            <p>Please select a joke from the list to transcribe and annotate it.</p>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator';
+// @ts-ignore
+import { Editor, EditorContent } from 'tiptap';
 
-@Component
+@Component({
+    components: {
+        EditorContent,
+    },
+})
 export default class JokeTranscriber extends Vue {
+
+    private editor = new Editor({
+        content: '<p></p>',
+    });
+
+    // ****************
+    // Lifecycle events
+    // ****************
+
+    public beforeDestroy() {
+        this.$data.editor.destroy();
+    }
 
     // **************
     // Event handlers
