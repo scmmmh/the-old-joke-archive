44898b4813d6ce0091ff1aa4fc77c8a894b9dd7f
Mark Hall <mark.hall@work.room3b.eu>
2020-01-27 13:59:31 +0000

    Completed initial source metadata fields

--- src/toja/config.py
+++ src/toja/config.py
@@ -1,8 +1,20 @@
 from jinja2 import Undefined
 
-SOURCE_METADATA = [{'name': 'pub_title', 'type': 'text', 'label': 'Publication'},
-                   {'name': 'pub_section', 'type': 'text', 'label': 'Section'},
-                   {'name': 'pub_date', 'type': 'date', 'label': 'Date'}]
+SOURCE_METADATA = [{'name': 'pub_type',
+                    'type': 'select',
+                    'values': [('newspaper', 'Newspaper'),
+                               ('book', 'Book')],
+                    'label': 'Publication Type'},
+                   {'name': 'pub_title', 'type': 'text', 'label': 'Publication Title'},
+                   {'name': 'pub_date', 'type': 'date', 'label': 'Publication Date (YYYY-MM-DD, YYYY-MM, or YYYY)'},
+                   {'name': 'pub_location', 'type': 'text', 'label': 'Publication Location'},
+                   {'name': 'pub_section', 'type': 'text', 'label': 'Column / Chapter Title'},
+                   {'name': 'pub_page_numbers', 'type': 'text', 'label': 'Page Numbers'},
+                   {'name': 'pub_publisher', 'type': 'text', 'label': 'Publisher'},
+                   ]
+SOURCE_SNIPPET_FIELD_TITLE = 'pub_title'
+SOURCE_SNIPPET_FIELD_SUBTITLE = 'pub_section'
+SOURCE_SNIPPET_FIELD_DATE = 'pub_date'
 JOKE_SNIPPET_FIELD_TITLE = 'source.pub_title'
 JOKE_SNIPPET_FIELD_SUBTITLE = 'source.pub_section'
 JOKE_SNIPPET_FIELD_DATE = 'pub_date'
@@ -18,6 +30,9 @@ SEARCH_FACETS = [{'name': 'pub_title', 'type': 'text', 'label': 'Publication'},
                  {'name': 'pub_date', 'type': 'date', 'label': 'Date'}]
 
 SETTINGS = {'SOURCE_METADATA': SOURCE_METADATA,
+            'SOURCE_SNIPPET_FIELD_TITLE': SOURCE_SNIPPET_FIELD_TITLE,
+            'SOURCE_SNIPPET_FIELD_SUBTITLE': SOURCE_SNIPPET_FIELD_SUBTITLE,
+            'SOURCE_SNIPPET_FIELD_DATE': SOURCE_SNIPPET_FIELD_DATE,
             'JOKE_SNIPPET_FIELD_TITLE': JOKE_SNIPPET_FIELD_TITLE,
             'JOKE_SNIPPET_FIELD_SUBTITLE': JOKE_SNIPPET_FIELD_SUBTITLE,
             'JOKE_SNIPPET_FIELD_DATE': JOKE_SNIPPET_FIELD_DATE,
