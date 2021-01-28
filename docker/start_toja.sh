6f5ad171e4fa31a0ef7d63b4199f2fa4887c2298
Mark Hall <mark.hall@work.room3b.eu>
2021-01-28 13:57:08 +0000

    Run docker as non-privileged user

--- docker/start_toja.sh
+++ docker/start_toja.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo $SQLALCHEMY_URL
+
+sed -i -e "s#sqlalchemy.url =.*#sqlalchemy.url = $SQLALCHEMY_URL#g" \
+       -e "s#app.session_secret =.*#app.session_secret = ${SESSION_SECRET}#g" \
+       -e "s#app.elasticsearch.hosts =.*#app.elasticsearch.hosts = ${ELASTICSEARCH_HOSTS}#g" \
+       -e "s#app.broker.url =.*#app.broker.url = ${BROKER_URL}#g" \
+       -e "s#app.email.smtp_host =.*#app.email.smtp_host = ${EMAIL_SMTP_HOST}#g" \
+       -e "s#app.email.ssl =.*#app.email.ssl = ${EMAIL_SMTP_SSL}#g" \
+       -e "s#app.email.username =.*#app.email.username = ${EMAIL_SMTP_USERNAME}#g" \
+       -e "s#app.email.password =.*#app.email.password = ${EMAIL_SMTP_PASSWORD}#g" \
+       -e "s#app.email.sender =.*#app.email.sender = ${EMAIL_SENDER}#g" /etc/toja/production.ini
+
 # Setup the database
 toja -c /etc/toja/production.ini init-db
 while [ $? -ne 0 ]
