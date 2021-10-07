166c5acc255edc3475562e6aab32420ab42c4eee
Mark Hall <mark.hall@work.room3b.eu>
2021-10-07 10:19:38 +0100

    Fix the popup menu on small screens

--- toja/server/frontend/src/components/Header.svelte
+++ toja/server/frontend/src/components/Header.svelte
@@ -1,7 +1,34 @@
 <script lang="ts">
+    import { tick } from 'svelte';
     import { Link } from 'svelte-navigator';
 
     import { breakpoint } from '../stores';
+
+    let menuVisible = false;
+    let popupList = null;
+    let showMenuButton = null;
+
+    async function showMenu() {
+        menuVisible = true;
+        await tick();
+        if (popupList) {
+            popupList.querySelector('a').focus();
+        }
+    }
+
+    async function hideMenu() {
+        menuVisible = false;
+        await tick();
+        if (showMenuButton) {
+            showMenuButton.focus();
+        }
+    }
+
+    function popupKeyUp(ev: KeyboardEvent) {
+        if (ev.key === 'Escape') {
+            hideMenu();
+        }
+    }
 </script>
 
 <header class="container mx-auto flex flex-col lg:flex-row mb-8">
@@ -17,13 +44,31 @@
                 <li class="flex-none" role="presentation"><Link to="/contribute" class="block px-8 py-1 font-blackriver-bold text-accent text-center">Contribute</Link></li>
                 <li class="flex-1" role="presentation"></li>
                 <li class="flex-none" role="presentation">
-                    <button class="block text-accent py-1 rounded focus:outline-primary">
+                    <button bind:this={showMenuButton} on:click={showMenu} class="block text-accent py-1 rounded focus:outline-primary" aria-label="Show the full menu">
                         <svg viewBox="0 0 24 24" class="w-6 h-6">
                             <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
                         </svg>
                     </button>
                 </li>
             </ul>
+            {#if menuVisible}
+                <div on:keyup={popupKeyUp} class="absolute left-0 top-0 w-full flex flex-row bg-white z-1 shadow-md">
+                    <ul bind:this={popupList} class="flex-auto">
+                        <li role="presentation"><Link to="/search" class="block px-8 py-2 font-blackriver-bold text-accent">Search</Link></li>
+                        <li role="presentation"><Link to="/contribute" class="block px-8 py-2 font-blackriver-bold text-accent">Contribute</Link></li>
+                        <li role="presentation"><Link to="/about" class="block px-8 py-2 font-blackriver-bold text-accent">About</Link></li>
+                        <li role="presentation"><Link to="/user/sign-up" class="block px-8 py-2 font-blackriver-bold text-accent">Sign Up</Link></li>
+                        <li role="presentation"><Link to="/user/log-in" class="block px-8 py-2 font-blackriver-bold text-accent">Log in</Link></li>
+                    </ul>
+                    <div>
+                        <button on:click={hideMenu} class="block text-accent py-1 rounded focus:outline-primary" aria-label="Hide the full menu">
+                            <svg viewBox="0 0 24 24" class="w-6 h-6">
+                                <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            {/if}
         {:else if $breakpoint <= 3}
             <ul class="flex flex-row">
                 <li class="flex-none" role="presentation"><Link to="/search" class="block px-8 py-1 font-blackriver-bold text-accent text-center">Search</Link></li>
