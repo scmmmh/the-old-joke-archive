5467bd7a82b0db64d8acb778d3b644468cad7f04
Mark Hall <mark.hall@work.room3b.eu>
2021-12-08 21:25:29 +0000

    Fix the e2e tests

--- tests/e2e/tests/basic/landing_page.ts
+++ tests/e2e/tests/basic/landing_page.ts
@@ -1,15 +1,14 @@
 import { Selector } from 'testcafe';
-import { request } from '../util';
+import { setupEmptyDatabase } from '../database';
 
 fixture('Landing Page').beforeEach(async (test) => {
-    await request('http://localhost:6543/tests/create');
     await test
         .navigateTo('http://localhost:6543/')
         .resizeWindow(1024, 768);
 });
 
 test('Has the app title', async (test) => {
+    await setupEmptyDatabase();
     await test
-        .expect(Selector('h1').innerText).eql('Find Something Funny')
-        .expect(Selector('h2').innerText).eql('The Old Joke Archive');
+        .expect(Selector('h1').innerText).eql('Welcome to The Old Joke Archive');
 });
