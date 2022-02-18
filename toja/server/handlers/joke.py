8c56ea5b7094c2c6e0ff653b3f834fd0022ae070
Mark Hall <mark.hall@work.room3b.eu>
2022-02-18 16:13:39 +0000

    Bit more work on getting updating jokes to work

--- toja/server/handlers/joke.py
+++ toja/server/handlers/joke.py
@@ -294,7 +294,8 @@ class JokeItemHandler(JSONAPIItemHandler):
                     },
                     'coerce': coerce_coordinates,
                 }
-            if (joke['status'] == 'extracted' and user['_id'] != joke['activity']['extracted']['user']):
+            if (joke['status'] == 'extracted' and user['_id'] != joke['activity']['extracted']['user']) \
+                    or allow_everything:
                 attributes['status'] = {
                     'type': 'string',
                     'required': False,
@@ -326,81 +327,37 @@ class JokeItemHandler(JSONAPIItemHandler):
                     'allowed': ['categories-verified'] if not allow_everything else ['extraction-verified',
                                                                                      'categories-verified']
                 }
-            schema = object_schema('jokes', iid, attributes, relationships)
-            obj = validate(schema, data, purge_unknown=True)
-            return obj
-        """schema = {
-            'type': {
-                'type': 'string',
-                'required': True,
-                'empty': False,
-                'allowed': ['jokes']
-            },
-            'id': {
-                'type': 'string',
-                'required': True,
-                'empty': False,
-                'allowed': [iid]
-            },
-            'attributes': {
-                'type': 'dict',
-                'required': True,
-                'schema': {
-                    'data': {
-                        'type': 'string',
-                        'required': False,
-                        'empty': False,
-                        'regex': r'data:image/(png|jpeg);base64,[a-zA-Z0-9+/=]+'
-                    }
-                }
-            },
-            'relationships': {
-                'type': 'dict',
-                'required': True,
-                'schema': {
-                    'source': one_to_one_relationship_schema('sources', value=source_id)
-                }
-            }
-        }
-        async with couchdb() as session:
-            db = await session['jokes']
-            doc = await db[iid]
-            if doc['status'] in ['extracted', 'extraction-verified'] or \
-                    'admin' in user['groups'] or 'editor' in user['groups']:
-                schema['attributes']['schema']['coordinates'] = {
-                    'type': 'list',
-                    'required': False,
-                    'empty': False,
-                    'minlength': 4,
-                    'maxlength': 4,
-                    'schema': {
-                        'type': 'number',
-                    },
-                    'coerce': coerce_coordinates,
-                }
-            if doc['status'] == 'auto-transcribed' or 'admin' in user['groups'] or 'editor' in user['groups']:
-                schema['attributes']['schema']['transcriptions'] = {
+            if joke['status'] == 'categories-verified' or allow_everything:
+                attributes['transcriptions'] = {
                     'type': 'dict',
                     'required': False,
-                    'empty': False,
                     'schema': {
                         user['_id']: {
-                            'type': 'dict'
+                            'type': 'dict',
                         }
                     }
                 }
-        obj = validate(schema, data, purge_unknown=True)
-        if 'coordinates' in obj['attributes']:
-            async with couchdb() as session:
-                db = await session['sources']
-                try:
-                    source = await db[obj['relationships']['source']['data']['id']]
-                except aio_exc.NotFoundError:
-                    raise ValidationError({'relationships.source': 'This source does not exist'})
-                source_attachment = Attachment(source, 'image')
-                with Image.open(BytesIO(await source_attachment.fetch())) as img:
-                    obj['attributes']['data'] = img.crop(obj['attributes']['coordinates'])
-        return obj"""
+                attributes['status'] = {
+                    'type': 'string',
+                    'required': False,
+                    'empty': False,
+                    'allowed': ['annotated'] if not allow_everything else ['extraction-verified',
+                                                                           'categories-verified',
+                                                                           'annotated']
+                }
+            if joke['status'] == 'annotated' and allow_everything:
+                attributes['status'] = {
+                    'type': 'string',
+                    'required': False,
+                    'empty': False,
+                    'allowed': ['annotations-verified'] if not allow_everything else ['extraction-verified',
+                                                                                      'categories-verified',
+                                                                                      'annotated',
+                                                                                      'annotations-verified']
+                }
+            schema = object_schema('jokes', iid, attributes, relationships)
+            obj = validate(schema, data, purge_unknown=True)
+            return obj
 
     async def create_put(self: 'JokeItemHandler', iid: str, data: dict, user: Union[Document, None]) -> Document:
         """Update a joke CouchDB document for a PUT request."""
@@ -409,16 +366,17 @@ class JokeItemHandler(JSONAPIItemHandler):
                 db = await session['jokes']
                 doc = await db[iid]
                 coords_changed = False
-                if 'type' in data['attributes']:
-                    doc['type'] = data['attributes']['type']
                 if 'coordinates' in data['attributes'] and doc['coordinates'] != data['attributes']['coordinates']:
                     doc['coordinates'] = data['attributes']['coordinates']
                     coords_changed = True
-                if coords_changed and 'transcriptions' in doc and 'auto' in doc['transcriptions']:
-                    del doc['transcriptions']['auto']
+                    if coords_changed and 'transcriptions' in doc and 'auto' in doc['transcriptions']:
+                        del doc['transcriptions']['auto']
                 if 'transcriptions' in data['attributes'] and user['_id'] in data['attributes']['transcriptions']:
                     doc['transcriptions'][user['_id']] = data['attributes']['transcriptions'][user['_id']]
-                if 'admin' in user['groups'] or 'editor' in user['groups']:
+                if 'status' in data['attributes']:
+                    doc['status'] = data['attributes']['status']
+
+                """if 'admin' in user['groups'] or 'editor' in user['groups']:
                     # Changes to save if the user is an editor or admin
                     if 'transcriptions' in data['attributes'] and user['_id'] in data['attributes']['transcriptions']:
                         doc['transcriptions']['final'] = data['attributes']['transcriptions'][user['_id']]
@@ -437,7 +395,7 @@ class JokeItemHandler(JSONAPIItemHandler):
                             'user': user['_id'],
                             'timestamp': datetime.utcnow().timestamp(),
                         }
-                        doc['status'] = 'transcription-verified'
+                        doc['status'] = 'transcription-verified'"""
                 doc['updated'] = datetime.utcnow().timestamp()
                 await doc.save()
                 if coords_changed and 'data' in data['attributes']:
