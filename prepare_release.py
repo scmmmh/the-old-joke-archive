83d73d37c479dcee77b49f0da1730b98dc28ec11
Mark <mark.hall@work.room3b.eu>
2023-09-22 16:58:01 +0100

    Update to v1.0.0b16 (#125)

--- prepare_release.py
+++ prepare_release.py
@@ -2,7 +2,7 @@
 import re
 
 
-VERSION = '1.0.0b15'
+VERSION = '1.0.0b16'
 
 
 def readlines(filename: str) -> list[str]:
@@ -19,6 +19,7 @@ def writelines(filename: str, lines: list[str]) -> None:
 
 def update_version(filename: str, pattern: str, version: str) -> None:
     """Update the version in the file."""
+
     def replace_version(line: str) -> str:
         if re.match(pattern, line):
             return re.sub(r'[0-9]+\.[0-9]+\.[0-9]+(?:[a-z0-9]+)?', version, line)
@@ -29,5 +30,5 @@ def update_version(filename: str, pattern: str, version: str) -> None:
 
 
 update_version('toja/server/frontend/package.json', r'^  "version": "[0-9]+\.[0-9]+\.[0-9]+(?:[a-z0-9]+)?",$', VERSION)
-update_version('pyproject.toml', r'^version = "[0-9]+\.[0-9]+\.[0-9]+(?:[a-z0-9]+)?"$',  VERSION)
+update_version('toja/__about__.py', r'^__version__ = "[0-9]+\.[0-9]+\.[0-9]+(?:[a-z0-9]+)?"$', VERSION)
 update_version('production/Dockerfile', r'^.*toja-[0-9]+\.[0-9]+\.[0-9]+(?:[a-z0-9]+)?-py3.*$', VERSION)
